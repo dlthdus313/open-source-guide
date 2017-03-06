93ddf16a667c392cc91fc40cfaf9748d27050816
Brandon Keepers <bkeepers@github.com>
2017-03-05 22:02:21 -0600

    Move all the scripts

--- js/index.js
+++ js/index.js
@@ -2,4 +2,14 @@
 ---
 
 {% include_relative jquery.min.js %}
+{% include_relative vendor/anchor-js/anchor.min.js %}
 {% include_relative script.js %}
+{% include_relative ga.js %}
+
+var selector = '.article-body h2, .article-body h3, .article-body h4, .article-body h5';
+anchors.options = {
+  placement: 'left'
+};
+anchors.add(selector);
+
+$(selector).wrapInner('<span/>');

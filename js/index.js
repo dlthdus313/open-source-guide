87520ec1382a970ab492d7ba9581cdc8dd3614ac
Brandon Keepers <bkeepers@github.com>
2017-03-05 22:19:15 -0600

    closure around anchor handlign

--- js/index.js
+++ js/index.js
@@ -6,10 +6,12 @@
 {% include_relative ga.js %}
 {% include_relative toc.js %}
 
-var selector = '.article-body h2, .article-body h3, .article-body h4, .article-body h5';
-anchors.options = {
-  placement: 'left'
-};
-anchors.add(selector);
+(function() {
+  var selector = '.article-body h2, .article-body h3, .article-body h4, .article-body h5';
+  anchors.options = {
+    placement: 'left'
+  };
+  anchors.add(selector);
 
-$(selector).wrapInner('<span/>');
+  $(selector).wrapInner('<span/>');
+})();
